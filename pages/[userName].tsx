df097d998212ba9ddec71750fe2bde5e389916db
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-12-29 17:01:51 +0900

    add userName page

---
+++ pages/[userName].tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+const userPage: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const { query, isReady, push } = useRouter()
+  const { userName } = query
+
+  useEffect(() => {
+      if (isReady) {
+          setLoading(true)
+      }
+  }, [isReady, userName])
+
+  if (!loading) return <p>Loading...</p>
+
+  if (!userName) {
+      push('/404')
+      return null
+  }
+
+  return (
+    <p>{userName}</p>
+  )
+}
+export default userPage
